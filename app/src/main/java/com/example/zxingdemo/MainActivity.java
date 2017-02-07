f257ad67856f97924d95b59b79460ac1bae49efb
william.zhang <zydqlg@sina.com>
2017-02-07 08:51:58 +0800

    ""

---
+++ app/src/main/java/com/example/zxingdemo/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.zxingdemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.xys.libzxing.zxing.activity.CaptureActivity;
+
+public class MainActivity extends Activity {
+    private Button scan;
+    public static int SCAN_RESULT = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        scan = (Button) findViewById(R.id.scan);
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+                startActivityForResult(intent, SCAN_RESULT);
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Toast.makeText(MainActivity.this, data.getStringExtra("result"), Toast.LENGTH_LONG).show();
+        if(resultCode == SCAN_RESULT){
+            Log.i("Mainactivity", "onActivityResult: " + data.toString());
+        }
+    }
+}
